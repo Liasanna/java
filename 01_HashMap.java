b69c1702e82dddfedd082ff9c12bfa1802c0a8ea
Anna <lias351@mail.ru>
2023-02-06 10:24:16 +0400

    0

--- 01_HashMap.java
+++ 01_HashMap.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Phonebook {
+public class 01_HashMap.java {
     private static HashMap<String, String> pb = new HashMap<String, String>();
 
     //addPB - добавляет запись по заданным номеру телефона и фамилии
